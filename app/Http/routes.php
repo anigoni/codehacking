96956c0f5de88714cddbeeb6f72caabb957d0807
anigoni <a.anigoni@gmail.com>
2017-09-13 14:32:43 +0200

    AdminUsersController added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +20,6 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('admin/users', 'AdminUsersController');
+

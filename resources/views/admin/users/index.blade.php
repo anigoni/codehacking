ffb85f58d94cf42f2e01b181bdb090d572d803a1
anigoni <a.anigoni@gmail.com>
2017-09-20 08:32:10 +0200

    workspace.xml committed from PHPStorm

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,38 @@
-<h1>index page for users is working!</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Admin index Users Page</h1>
+
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Created_At</th>
+            <th>Updated_At</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @if($users)
+
+            @foreach($users as $user)
+            <tr>
+                <td>{{$user->id}}</td>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                <td>{{$user->created_at->diffForHumans()}}</td>
+                <td>{{$user->upted_at}}</td>
+            </tr>
+            @endforeach
+
+        @endif
+
+
+        </tbody>
+    </table>
+
+@endsection
+
